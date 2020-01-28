fa67fa0e5cdb60aaf06153e7888d188a15926576
dayoung-lee <dayoung5213@gmail.com>
2020-01-28 04:28:29 -0500

    Updates

---
+++ precache-manifest.ccf014457c4fd9840db5955d409147ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e01f8652ce34cb9be55b58c0f764015",
+    "url": "/restaurant-app/index.html"
+  },
+  {
+    "revision": "f8e799781938a5770c09",
+    "url": "/restaurant-app/static/css/main.b536fa4e.chunk.css"
+  },
+  {
+    "revision": "534510bdbca877ba2c5f",
+    "url": "/restaurant-app/static/js/2.2a8deace.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/restaurant-app/static/js/2.2a8deace.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8e799781938a5770c09",
+    "url": "/restaurant-app/static/js/main.ff265f3d.chunk.js"
+  },
+  {
+    "revision": "f68840457374e61ab4a2",
+    "url": "/restaurant-app/static/js/runtime-main.ab8f4000.js"
+  },
+  {
+    "revision": "4d9a5a88fd38d09e3ac477d0f21cdb06",
+    "url": "/restaurant-app/static/media/aboutImg1.4d9a5a88.jpg"
+  },
+  {
+    "revision": "3ffcb51767073b886764d659ee399a41",
+    "url": "/restaurant-app/static/media/appetizer.3ffcb517.png"
+  },
+  {
+    "revision": "25be68f7db515bfcf2bf56e207e56fa6",
+    "url": "/restaurant-app/static/media/main_noodle1.25be68f7.jpg"
+  },
+  {
+    "revision": "75d7b1d8e8d2cdc94b18748bbf9a47eb",
+    "url": "/restaurant-app/static/media/menu_img.75d7b1d8.jpg"
+  },
+  {
+    "revision": "3a6e367060e8c4ba02a1d5f0d34b0007",
+    "url": "/restaurant-app/static/media/review.3a6e3670.jpg"
+  }
+]);
